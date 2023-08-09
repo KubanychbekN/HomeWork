ea6963c822fb331f4e1342dcd2ac71ca28a699bc
KubanychbekN <nurdinovkuba5@gmail.com>
2023-08-09 20:10:34 +0600

    'git'

--- hero.py
+++ hero.py
@@ -33,4 +33,71 @@ hero.info()
 catchphrase_length = hero.length_catchphrase()
 print("Length of catchphrase:", catchphrase_length)
 
-print()
+print('-----------------')
+
+class AirHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase,fly, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
+        self.fly = False
+
+    def squaring_health(self):
+        self.health_points **= 2
+        self.fly = True
+
+    def fly_phrase(self):
+            return f"fly in the True_phrase"
+
+
+
+
+airhero = AirHero("James Rhodes", "Iron Patriot", "iron man suit", 80, "I am America's protector", "Sky", 50)
+
+airhero.info()
+airhero.squaring_health()
+print("Health points:", airhero.health_points)
+print("Fly:", airhero.fly)
+print(airhero.fly_phrase())
+
+print('-----------------')
+
+class EarthHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase,fly, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
+        self.fly = True
+
+    def squaring_health(self):
+        self.health_points **= 2
+        self.fly = True
+
+    def fly_phrase(self):
+            return f"fly in the True_phrase"
+
+
+
+
+
+earthero= EarthHero("Steve Rogers", "Captain America", "Super Soldier", 200, "I am captain america", "Land", 100)
+
+earthero.info()
+earthero.squaring_health()
+print("Health points:", earthero.health_points)
+print("Fly:", earthero.fly)
+print(earthero.fly_phrase())
+
+print('-----------------')
+
+class Villain(AirHero):
+    people = "Monster"
+
+    def gen_x(self):
+        pass
+
+    def crit(self,degree):
+        return self.damage ** degree
+
+
+villain = Villain("Dark Matter", "Voidbringer", "Dark energy manipulation", 250, "Into the shadows", "Unknown", 50)
